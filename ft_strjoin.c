f30b335fba3bda596eff2b1d132d0458cda05238
Olorunfemi Durosinmi <oldurosi@e1z3r7p21.42.us.org>
2019-10-21 02:23:06 -0700

    libft reloaded

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -11,28 +11,19 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*result;
-	size_t	size;
+	int		strlen;
+	char	*str;
 
-	if (s1 && s2)
-		size = (size_t)(ft_strlen((char*)s1) + ft_strlen((char*)s2));
-	else if (s1)
-		size = (size_t)(ft_strlen((char*)s1));
-	else if (s2)
-		size = (size_t)(ft_strlen((char*)s2));
-	else
+	if (!s1 || !s2)
 		return (NULL);
-	if (!(result = ft_memalloc(size)))
+	strlen = ft_strlen(s1) + ft_strlen(s2);
+	str = ft_strnew(strlen);
+	if (str == NULL)
 		return (NULL);
-	if (s1)
-		result = ft_strcpy(result, (char*)s1);
-	else
-		result = ft_strcpy(result, (char*)s2);
-	if (s1 && s2)
-		result = ft_strcat(result, (char*)s2);
-	return (result);
+	ft_strcpy(str, s1);
+	ft_strcat(str, s2);
+	return (str);
 }
